9cb7a4d1285205cb22d3d86318ab6ec33404ebc6
mdudak <mato.dudak@gmail.com>
2024-10-22 23:32:16 +0200

    Removed install.sh

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/test', [TesterController::class, 'runTest']);
 
 Route::get('/test', function () {
-    return response('run "curl -X POST -F \'file=@your_file.c\' https://prpr.filo.h10s.eu/test to test your app');
+    return response('run "curl -X POST -F \'file=@your_file.c\' https://prpr.filo.h10s.eu/api/test" to test your app');
 });
