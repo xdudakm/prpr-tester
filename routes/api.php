29b261f1949865993d9cd9d712bfa00d94b1dc7a
mdudak <mato.dudak@gmail.com>
2024-10-22 18:23:33 +0200

    Added GET endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\TesterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/test', [TesterController::class, 'runTest']);
+
+Route::get('/test', function () {
+    return response('run "curl -X POST -F \'file=@your_file.c\' https://prpr.filo.h10s.eu/test to test your app');
+});
