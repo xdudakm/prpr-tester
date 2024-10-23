d0bf63f140fdf4a31f9feb44b957c9848cf17e4d
mdudak <mato.dudak@gmail.com>
2024-10-23 18:44:48 +0200

    Testing

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\TesterController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/test', [TesterController::class, 'runTest']);
+Route::post('/test', [TesterController::class, 'runTest'])->name('test');
 Route::get('/results/{fileName}', [TesterController::class, 'getResults'])->name('results');
 
 Route::get('/test', function () {
-    return response('run "curl -X POST -F \'file=@your_file.c\' https://prpr.filo.h10s.eu/api/test" to test your app');
+    return response('run "curl -X POST -F \'file=@your_file.c\' ' . \route('test') . ' " to test your app');
 });
