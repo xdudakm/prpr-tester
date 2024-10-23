3f086ccb5e698aa726f4d1978d9977d16465e523
mdudak <mato.dudak@gmail.com>
2024-10-24 00:45:41 +0200

    Added artisan commands

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 php artisan migrate --force
+php artisan cache:clear
+php artisan view:clear
+php artisan config:cache
+php artisan route:cache
+php artisan queue:restart
 chmod +777 database/database.sqlite
 
 chmod +777 ./storage -R
