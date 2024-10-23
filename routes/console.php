b91a29c66f7cb7b206b0e93f5b3df7f062188cfe
mdudak <mato.dudak@gmail.com>
2024-10-23 20:06:56 +0200

    Testing

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::exec('find /var/www/http/storage/public/results -type f -mmin +1440 -exec rm -f {} +')->hourly();
