10573f1ffa88d48018cfbdc38fcaca77a55737de
mdudak <mato.dudak@gmail.com>
2024-11-02 23:02:20 +0100

    Corrected points

--- config/app.php
+++ config/app.php
@@ -3,7 +3,8 @@
 return [
 
     'supported_functions' => [
-        'v1', 'v2', 'v3', 'v-ine', 'h', 'n', 'q', 'w', 'e', 'm', 'a', 's', 'd','k'
+//        'v1', 'v2', 'v3', 'v-ine', 'h', 'n', 'q', 'w', 'e', 'm', 'a', 's', 'd','k'
+        'v1', 'v2', 'v-ine', 'h', 'n', 'q', 'w', 'e','k'
     ],
 
     /*
